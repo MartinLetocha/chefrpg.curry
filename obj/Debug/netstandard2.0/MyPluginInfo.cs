9c48e4b35da7df4dce795c9a309001e367743a28
MartinLetocha <martinletocha2@gmail.com>
2025-06-29 16:47:34 +0200

    Everything

---
+++ obj/Debug/netstandard2.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CurryRPG
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "dependentspiritual.chefrpg.curry";
+        public const string PLUGIN_NAME = "Curry";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
